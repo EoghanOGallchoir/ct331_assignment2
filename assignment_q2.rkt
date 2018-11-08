1eb867632872904790704bdfb6fe1f1f0b28ba42
EoghanOGallchoir <e.ogallchoir1@nuigalway.ie>
2018-11-08 17:32:32 +0000

    q.2 (F) and q.3 (E&F) missing

--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -11,7 +11,77 @@
 ;
 ;You may delete these comments!
 
+
+;A
 (provide ins_beg)
 
 (define (ins_beg el lst)
-  (display "Hello, I'm ins_beg!\n"))
+        (cons el lst))
+
+;B
+(provide ins_end)
+
+(define (ins_end el lst)
+  (append lst (list el)))
+
+;C
+(provide count_top_level)
+
+(define (count_top_level lst)
+  (if
+   (null? lst)
+   0
+   (+ 1 (count_top_level (cdr lst)))))
+
+;D
+(provide count_instances)
+
+(define (count_instances lst item)
+    (cond (
+           (empty? lst)
+           0)
+          ((= (car lst) item)
+           (+ 1 (count_instances (cdr lst) item)))
+          (else
+           (count_instances (cdr lst) item)))
+  )
+
+;E
+(provide count_instances_tr)
+
+(define (count_instances_tr lst item)
+  (calculate_tr item lst 0))
+(define (calculate_tr item lst total)
+  (cond
+    [(empty? lst) total]
+    [(equal? item (car lst))
+     (calculate_tr item (cdr lst) (+ 1 total))]
+    [else (calculate_tr item
+                        (cdr lst) total)]))
+
+
+
+;A.
+(display "(A) The ins_beg:\n")
+(ins_beg 'a '(b c d))
+
+;B.
+(display "(B) ins_end:\n")
+(ins_end '(a b) '(b c d))
+
+;C.
+(display "(C) count_top_level:\n")
+(count_top_level '(1 2 3 (4 5 6)))
+
+;D.
+(display "(D) count_instances:\n")
+(count_instances '(1 2 3 4 4) 4)
+
+;E
+(display "(E) count_instances_tr:\n")
+(count_instances_tr '(1 2 3 4 4)4)
+
+
+
+
+
--- assignment_q3.rkt
+++ assignment_q3.rkt
@@ -1,1 +1,65 @@
 #lang racket
+
+(define (left_tree tree)
+         (car tree))
+
+(define (right_tree tree)
+         (car tree))
+
+(define (value_tree tree)
+         (car tree))
+
+
+
+;A
+(define (sort_tree tree)
+ (begin(cond [(not (empty?(left_tree tree))) (sort_tree (left_tree tree))])
+   (printf "~a " (value_tree tree))
+   (cond [(not (empty?(right_tree tree))) (sort_tree (right_tree tree))]))
+  )
+  
+
+;B
+(define (search el tree)
+(cond
+  [(null? tree) #f]
+  [(equal? tree (left_tree tree) )#t]
+  [else (search el (right_tree tree) )])
+  
+  )
+
+;C
+(define (insert el tree)
+  (cond ((empty? tree) (list '() el '()))
+        ((equal? el (cadr tree)) tree)
+        ((< el (cadr tree))
+         (list (insert el(left_tree tree)) (cadr tree) (right_tree tree)))
+        (else (list (left_tree tree) (cadr tree) (insert el (right_tree tree)))))
+  )
+
+;D
+(define (insert_list el tree)
+ (if(empty? el) tree
+    (insert_list (cdr el) (insert (car el) tree)))
+  )
+
+(define bst '(((() 2 ()) 14 (() 6 ())) 5 ((() 4 ()) 12 (() 10 ()))))
+
+;Part A
+(display "(A) sorted:\n")
+(sort_tree bst)
+
+(display "\n")
+
+ ;Part B
+(display "(B) Searching:\n")
+(search 7 bst) ;#f
+(search 2 bst) ;#t
+
+ ;Part C
+(display "(C) Insert Element (1):\n")
+(insert 1 bst)
+
+ ;Part D
+(display "(D) Insert list (4 9):\n")
+(insert_list '(4 9 14)bst)
